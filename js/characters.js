0a747ccd60bb2b28894c3a3cf2333b6f5f1d958d
Pedro Castaneda <petecast76@gmail.com>
2024-01-13 12:37:23 -0800

    feat: changed bg img

--- js/characters.js
+++ js/characters.js
@@ -10,10 +10,11 @@ const starFighter = document.getElementById('starFighter');
 // Global variables
 injectHere.innerHTML = '<div class="row"><div class="col-12 d-flex flex-column align-items-center floater"><img src="../assets/images/icons8-baby-yoda-144.png" alt="Baby Yoda Loading Icon"><p class="text-white font-monospace fs-2">Loading...</p></div></div>';
 let data = await GetData();
+console.log(data);
 let allCharacters = await GetAllCharacters(data.people);
 injectHere.innerHTML = '';
-// console.log('All Characters func starts with...');
-// console.log(allCharacters);
+console.log('All Characters func starts with...');
+console.log(allCharacters);
 
 // Event Listeners
 starFighter.addEventListener('click', function(){
